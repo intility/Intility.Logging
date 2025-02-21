a510f7d02c0125d9c90e570bb107594fb291e7db
Thorstein Løkensgard <thorstein.lokensgard@intility.no>
2025-02-21 09:50:18 +0100

    feat: add new pipeline

--- src/Intility.Extensions.Logging/LoggerBuilder.cs
+++ src/Intility.Extensions.Logging/LoggerBuilder.cs
@@ -3,23 +3,13 @@ using Serilog;
 
 namespace Intility.Extensions.Logging
 {
-    public class LoggerBuilder : ILoggerBuilder
+    public class LoggerBuilder(HostBuilderContext host, LoggerConfiguration config, IHostBuilder hostBuilder) : ILoggerBuilder
     {
-        private readonly HostBuilderContext _host;
-        private readonly LoggerConfiguration _config;
-        private readonly IHostBuilder _hostBuilder;
         public ConsoleFormat ConsoleFormat { get; private set; } = ConsoleFormat.Pretty;
 
-        public LoggerBuilder(HostBuilderContext host, LoggerConfiguration config, IHostBuilder hostBuilder)
-        {
-            _host = host;
-            _config = config;
-            _hostBuilder = hostBuilder;
-        }
-
-        public HostBuilderContext Host => _host;
-        public IHostBuilder HostBuilder => _hostBuilder;
-        public LoggerConfiguration Configuration => _config;
+        public HostBuilderContext Host => host;
+        public IHostBuilder HostBuilder => hostBuilder;
+        public LoggerConfiguration Configuration => config;
 
         public ILoggerBuilder UseConsoleFormat(ConsoleFormat format)
         {
